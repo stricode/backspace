057c1892c4cbaba66eaa0e4a5a83ee5aeb650bec
Stricode Mobile <nextcode10@gmail.com>
2015-12-27 13:41:46 +0530

    Initial commit of www

---
+++ www/scripts/main.js
@@ -0,0 +1,23 @@
+var editor = document.getElementById('editor');
+var languages = ["english", "urdu"];
+var kb = new Keyboard(editor, true, languages);
+kb.Hide();
+
+var isShown = false;
+function toggleKeyboard() {
+    if (isShown) {
+        kb.Hide();
+        isShown = false;
+    }
+    else {
+        kb.Show();
+        isShown = true;
+    }
+}
+
+var button = document.getElementById("toggleButton");
+button.addEventListener("click", function () {
+    toggleKeyboard();
+    var prefix = !isShown ? "View" : "Hide";
+    button.innerText = prefix + " Keyboard";
+});
